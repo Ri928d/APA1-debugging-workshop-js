b91784c9f7c3300b7e95ffdd94563f5f3a967971
Ri928d <richard.segbenu@ada.ac.uk>
2025-01-13 12:41:03 +0000

    Capitalise tests passed

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,7 +1,12 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+  //word[0] = word[0].toUpperCase();
+
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  
 }
+  
+
+
 
 module.exports = { capitalise };
 
